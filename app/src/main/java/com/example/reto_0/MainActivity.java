7da0559e7a7f75a9145166fe9ab24426fca373f2
Sebastian <jsneisav@unal.edu.co>
2022-08-22 10:47:26 -0500

    First try to make a commit from android studio

---
+++ app/src/main/java/com/example/reto_0/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.reto_0;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.reto_0.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
